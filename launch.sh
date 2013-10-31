f930b5e87f47e735a88acd18da664f24ff7647a7
Devin Weaver <suki@tritarget.org>
2013-10-31 09:54:06 -0400

    Adds debug option for launching [skip ci]

--- launch.sh
+++ launch.sh
@@ -5,7 +5,11 @@ server=$!
 
 trap "kill $server" KILL
 
-titanium build --platform ios --ios-version 7.0 --retina --tall --log-level debug
+if [ "$1" = "-d" ]; then
+  DEBUG="--log-level debug"
+fi
+
+titanium build --platform ios --ios-version 7.0 --retina --tall $DEBUG
 
 echo
 echo "killing server ($server)"
