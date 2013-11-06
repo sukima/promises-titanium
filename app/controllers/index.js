bd42b17105bffb796cfbdde064a611f13d8349f2
Devin Weaver <suki@tritarget.org>
2013-11-06 11:17:00 -0500

    Adds the buttons to the main screen

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1,5 +1,18 @@
-function doClick(e) {
-    alert($.label.text);
+function buildHttpButtons() {
+	var i, len, button;
+
+	var httpButtons = [
+		{subtitle: "success",          href: "http://localhost:3000/good_json", login: false},
+		{subtitle: "parse error",      href: "http://localhost:3000/bad_json",  login: false},
+		{subtitle: "not found",        href: "http://localhost:3000/not_found", login: false},
+		{subtitle: "needs user input", href: "http://localhost:3000/login",     login: true}
+	];
+
+	for (i = 0, len = httpButtons.length; i < len; i++) {
+		button = Alloy.createController('http_button', httpButtons[i]);
+		$.index.add(button.getView());
+	}
 }
 
+buildHttpButtons();
 $.index.open();
