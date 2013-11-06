b84ccb1406679b0e05b7b65bd6bc581f61b244d7
Devin Weaver <suki@tritarget.org>
2013-11-06 08:27:08 -0500

    Updates launch.sh

--- launch.sh
+++ launch.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 
-coffee server.coffee &
-server=$!
+APP_ID=org.tritarget.wnlmobile.app
+# Must be capitalized!
+APP_NAME=WNLMobile
 
-trap "kill $server" KILL
-
+PATH=node_modules/.bin:$PATH
 while test $# -gt 0; do
     case "$1" in
+        -i)
+            install="yes"
+            ;;
         -d)
             DEBUG="--log-level debug"
             ;;
@@ -14,12 +17,21 @@ while test $# -gt 0; do
             # Support ti-inspector
             remote="--debug-host localhost:8999"
             ;;
+        -a)
+            android="yes"
+            ;;
     esac
 
     shift
 done
 
-titanium build --platform ios --ios-version 7.0 --retina --tall $DEBUG $remote
-
-echo
-echo "killing server ($server)"
+if [ "$install" = "yes" ]; then
+  exec titanium build -p ios --ios-version 7.0 -T device
+elif [ "$android" = "yes" ]; then
+  titanium build --platform android --build-only --avd-id 1 $DEBUG && \
+  adb uninstall $APP_ID
+  adb install build/android/bin/app.apk && \
+  adb shell am start ${APP_ID}/.${APP_NAME}Activity
+else
+  exec titanium build --platform ios --ios-version 7.0 --retina --tall $DEBUG $remote
+fi
